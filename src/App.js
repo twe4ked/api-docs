c903dac7741933652568ea7aabea9c1b37cb5017
Odin Dutton <odindutton@gmail.com>
2016-12-30 11:00:41 +1100

    Extract Example component

--- src/App.js
+++ src/App.js
@@ -62,6 +62,39 @@ const ResponseFields = ({responseFields}) => {
   )
 }
 
+const Example = ({example}) => {
+  return (
+    <section>
+      <div className="left">
+        <div className="padding">
+          <h2 id={example.name.toLowerCase().replace(/[^a-z]/g, '-')}>{example.name}</h2>
+          { !!example.description &&
+            <SimpleFormat text={example.description} />
+          }
+          <code className="path">
+            {example.httpMethod} <strong>{example.path}</strong>
+          </code>
+          { !!example.parameters.length && <Parameters parameters={example.parameters} /> }
+          { !!example.responseFields.length && <ResponseFields responseFields={example.responseFields} /> }
+        </div>
+      </div>
+
+      <div className="right">
+        <div className="padding">
+          {example.requests.map((request, index) =>
+            <div key={index}>
+              <code>{`${request.request_method} ${request.request_path}`}</code>
+              <SyntaxHighlighter language='json' style={irBlack}>
+                {JSON.stringify(JSON.parse(request.response_body), null, 2)}
+              </SyntaxHighlighter>
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
+  )
+}
+
 const Resource = ({resource}) => {
   return (
     <div>
@@ -74,33 +107,7 @@ const Resource = ({resource}) => {
         </div>
       </div>
       {resource.examples.map((example, index) =>
-        <section key={index}>
-          <div className="left">
-            <div className="padding">
-              <h2 id={example.name.toLowerCase().replace(/[^a-z]/g, '-')}>{example.name}</h2>
-              { !!example.description &&
-                <SimpleFormat text={example.description} />
-              }
-              <code className="path">
-                {example.httpMethod} <strong>{example.path}</strong>
-              </code>
-              { !!example.parameters.length && <Parameters parameters={example.parameters} /> }
-              { !!example.responseFields.length && <ResponseFields responseFields={example.responseFields} /> }
-            </div>
-          </div>
-          <div className="right">
-            <div className="padding">
-              {example.requests.map((request, index) =>
-                <div key={index}>
-                  <code>{`${request.request_method} ${request.request_path}`}</code>
-                  <SyntaxHighlighter language='json' style={irBlack}>
-                    {JSON.stringify(JSON.parse(request.response_body), null, 2)}
-                  </SyntaxHighlighter>
-                </div>
-              )}
-            </div>
-          </div>
-        </section>
+        <Example key={index} example={example} />
       )}
     </div>
   )
